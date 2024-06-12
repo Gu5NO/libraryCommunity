1a31018445f3b8c23cfdb25bdc7bdc4bea44285f
Gustavo Nieves <gus.mix.ss@gmail.com>
2024-06-11 19:29:26 -0600

    Archivo de conexión a la bd

---
+++ backend/config/connection/conexion.php
@@ -0,0 +1,30 @@
+<?php
+    class conexion{    
+        private $host;
+        private $dbname;
+        private $username;
+        private $password;
+        private $charset;
+        private $pdo;
+        public function __construct()
+        {
+            $this->host         = "localhost";
+            $this->dbname       = "libraryCommunity";
+            $this->username     = "root";
+            $this->password     = "";
+            $this->charset      = "utf8";
+            
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset={$this->charset}";
+
+            try {
+                $this->pdo = new PDO($dsn, $this->username, $this->password);
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Error de conexión: " . $e->getMessage());
+            }
+        }
+        public function connect()
+        {
+            return $this->pdo;
+        }
+    }
